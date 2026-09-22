7b60bf96d6f840d1af16a1b5a85f60d435df93b7
Marcus1107 <144109740+Marcus1107@users.noreply.github.com>
2026-09-22 10:10:56 +0200

    Initiale Migration für Tickets erstelltt

--- INSY/TicketConcurrency/WebAPI/Program.cs
+++ INSY/TicketConcurrency/WebAPI/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// Beispieldaten im Arbeitsspeicher; noch keine Datenbank.
+
 var tickets = Enumerable.Range(1, 100)
     .Select(id => new Ticket { Id = id, Name = $"Ticket {id}" })
     .ToArray();
